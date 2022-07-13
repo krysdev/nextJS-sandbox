349d479e11889a45fccb98c589b6598d4138d9c8
KrysDev <webdevkrys@gmail.com>
2022-07-13 22:14:16 +0100

    project

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,31 @@
+import fs from 'fs/promises'
+
 function HomePage(props) {
-  
+  const {products} = props
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {
+        products.map((listel)=>(
+          <li key={listel.id}>{listel.title}</li>
+        ))
+      }
+
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  await fs.readFile()
+  return {
+    props: {
+      products: [
+        { id: "p1", title: "Product 1", description: "This is product 1" },
+        { id: "p2", title: "Product 2", description: "This is product 2" },
+        { id: "p3", title: "Product 3", description: "This is product 3" },
+      ],
+    },
+  };
 }
 
 export default HomePage;
