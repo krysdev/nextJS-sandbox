3d559aec4277fe4184c126c0156b708acd38590d
KrysDev <webdevkrys@gmail.com>
2022-07-14 20:02:02 +0100

    project

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,14 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log("Generating")
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
   return {
-    props: {
-      products: data.products
-      
-      // [
-      //   { id: "p1", title: "Product 1", description: "This is product 1" },
-      //   { id: "p2", title: "Product 2", description: "This is product 2" },
-      //   { id: "p3", title: "Product 3", description: "This is product 3" },
-      // ],
-    },
+    props: { products: data.products },
+    revalidate: 10,
   };
 }
 
