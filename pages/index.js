69e28413890b4eaf9b55419808229663ec92209c
KrysDev <webdevkrys@gmail.com>
2022-08-03 21:33:26 +0100

    project

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,36 @@
+import { useRef } from "react";
+
 function HomePage() {
-  
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitForm(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    fetch("/api/feedback", {
+      method: "POST",
+      body: ''
+    });
+  }
+
   return (
     <div>
       <h1>Home Page</h1>
+      <form onSubmit={submitForm}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send feedback</button>
+      </form>
     </div>
   );
 }
 
-export default HomePage
+export default HomePage;
